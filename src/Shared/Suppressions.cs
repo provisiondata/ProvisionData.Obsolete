4766b1cf3f1c8304f20f898e00c52d468b7502a8
Doug Wilson <d@dkw.io>
2019-09-21 11:19:20 -0700

    More work on Nuke.Build

---
+++ src/Shared/Suppressions.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+// General
+[assembly: SuppressMessage("Simplification", "RCS1021:Simplify lambda expression.", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "ProvisionData.SecureShare.Migrations")]
+[assembly: SuppressMessage("Style", "IDE0011:Add braces", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "ProvisionData.SecureShare")]
+[assembly: SuppressMessage("Style", "IDE0049:Use framework type", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "ProvisionData.SecureShare.Migrations")]
+[assembly: SuppressMessage("Redundancy", "RCS1036:Remove redundant empty line.", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "ProvisionData.SecureShare.Migrations")]
+[assembly: SuppressMessage("Design", "CA1034:Nested types should not be visible", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "ProvisionData.SecureShare.Features")]
+[assembly: SuppressMessage("Style", "IDE0011:Add braces", Justification = "<Pending>")]
+
+// Specific
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:ProvisionData.SecureShare.Startup.Configure(Microsoft.AspNetCore.Builder.IApplicationBuilder,Microsoft.AspNetCore.Hosting.IHostingEnvironment)")]
+
+// Unit Testing
+[assembly: SuppressMessage("Style", "VSTHRD200:Use \"Async\" suffix for async methods", Justification = "<Pending>", Scope = "namespaceanddescendants", Target = "ProvisionData.SecureShare.Tests")]
